51681a7f7a8601f0ec79e8049f2c93b6c3ee174c
tarheeljessicajones <tarheeljessicajones@gmail.com>
2017-09-13 11:54:54 -0400

    changed layout

--- train.js
+++ train.js
@@ -21,6 +21,7 @@ $("#addTrainBtn").on("click",function(){
 	var firstTrain = moment($("#firstTrainInput").val().trim(), "HH:mm").subtract(10,"years").format("x");
 	var frequency = $("#frequencyInput").val().trim();
 
+
 	//create new train entry for database
 	var newTrain = {
 		name: trainName,
@@ -39,5 +40,35 @@ $("#addTrainBtn").on("click",function(){
 	$("#trainNameInput").val("");
 	$("#destinationInput").val("");
 	$("#firstTrainInput").val("");
+	$("#frequencyInput").val("");
 
+})
+
+//on submit, clear form data
+$("#clearBtn").on("click",function(){
+	event.preventDefault();
+	$("#trainNameInput").val("");
+	$("#destinationInput").val("");
+	$("#firstTrainInput").val("");
+	$("#frequencyInput").val("");
+})
+
+//whenever database updates, gather last train entry details
+trainData.ref().on("child_added", function(snapshot){
+	var name = snapshot.val().name;
+	var destination = snapshot.val().destination;
+	var frequency = snapshot.val().frequency;
+	var firstTrain = snapshot.val().firstTrain;
+
+	//calcuate enxt arrival by initial time divided by arrival frequency
+	var remainder = moment().diff(moment.unix(firstTrain),"minutes")%frequency;
+	var minutes = frequency - remainder;
+	var arrival = moment().add(minutes,"m").format("hh:mm A");
+	
+	console.log(remainder);
+	console.log(minutes);
+	console.log(arrival);
+
+	//display these details into the database
+	$("#trainTable > tBody").append("<tr><td>"+name+"</td><td>"+ destination+"</td><td>"+frequency+"</td><td>"+arrival+"</td><td>"+minutes+"</td></tr>");
 })
